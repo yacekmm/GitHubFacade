59682ef90a4b6189d5ac7b74b2a1d88b0bcfef9d
Jacek Milewski <jacek.milewski@orange.com>
2016-05-10 20:17:39 +0200

    integration tests task

---
+++ GitHubFacadeService/src/integration-test/java/com/looksok/application/GitHubFacadeServiceIntegrationTest.java
@@ -0,0 +1,36 @@
+package com.looksok.application;
+
+import com.jayway.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GitHubFacadeServiceApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+public class GitHubFacadeServiceIntegrationTest {
+
+	@Value("${local.server.port}")
+	int port;
+
+    @Before
+    public void setUp() throws Exception {
+        RestAssured.port = port;
+    }
+
+    @Test
+	public void requestsSampleRepo() {
+        RestAssured.when()
+                .get("/api")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+	}
+
+}
