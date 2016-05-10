8e1b43e0c264329cc7bd81a8c06df930a5966fa3
Jacek Milewski <jacek.milewski@orange.com>
2016-05-10 21:14:34 +0200

    building target url

--- GitHubFacadeService/src/main/java/com/looksok/service/repo/details/RepoDetailsService.java
+++ GitHubFacadeService/src/main/java/com/looksok/service/repo/details/RepoDetailsService.java
@@ -1,13 +1,13 @@
 package com.looksok.service.repo.details;
 
+import com.looksok.constants.ConstAppLogic;
 import com.looksok.service.rest.RestTemplatePrototype;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 @Component
@@ -15,9 +15,20 @@ public class RepoDetailsService {
 
     private RestTemplatePrototype restTemplatePrototype;
 
+    @Autowired
+    public RepoDetailsService(RestTemplatePrototype restTemplatePrototype) {
+        this.restTemplatePrototype = restTemplatePrototype;
+    }
+
     public Optional<ResponseEntity<RepoDetailsDto>> requestRepoDetails(String ownerUsername, String repoName){
-        System.out.println(restTemplatePrototype);
-        throw new NotImplementedException();
+        URI targetUrl = UriComponentsBuilder.fromUriString(ConstAppLogic.GitHubUrl.REPOS)
+                .path(ownerUsername)
+                .path("/")
+                .path(repoName)
+                .build(true).toUri();
+
+        restTemplatePrototype.getRestTemplate().getForObject(targetUrl, String.class);
+        return Optional.empty();
     }
 
 }
--- GitHubFacadeService/src/test/groovy/com/looksok/service/repo/details/RepoDetailsServiceSpec.groovy
+++ GitHubFacadeService/src/test/groovy/com/looksok/service/repo/details/RepoDetailsServiceSpec.groovy
@@ -1,9 +1,37 @@
 package com.looksok.service.repo.details
 
+import com.looksok.constants.ConstAppLogic
+import com.looksok.service.rest.RestTemplatePrototype
+import org.springframework.web.client.RestTemplate
 import spock.lang.Specification
 
 class RepoDetailsServiceSpec extends Specification {
-    def "RequestRepoDetails"() {
 
+    private RepoDetailsService repoDetailsService
+    private RestTemplatePrototype restTemplatePrototypeMock
+    private RestTemplate restTemplateMock
+
+    void setup() {
+        restTemplatePrototypeMock = Mock()
+        restTemplateMock = Mock()
+        repoDetailsService = new RepoDetailsService(restTemplatePrototypeMock)
+
+        restTemplatePrototypeMock.getRestTemplate() >> restTemplateMock
+    }
+
+    def "RequestRepoDetails executes GET on restTemplate"() {
+        given:
+        String expectedUser = "user"
+        String expectedRepo = "repo"
+        def expectedUri = new URI(ConstAppLogic.GitHubUrl.REPOS + expectedUser + "/" + expectedRepo)
+
+        when:
+        repoDetailsService.requestRepoDetails(expectedUser, expectedRepo)
+
+        then:
+        1 * restTemplatePrototypeMock.getRestTemplate() >> restTemplateMock
+        1 * restTemplateMock.getForObject(_, _) >> {actualUrl, type ->
+            assert actualUrl == expectedUri
+        }
     }
 }
