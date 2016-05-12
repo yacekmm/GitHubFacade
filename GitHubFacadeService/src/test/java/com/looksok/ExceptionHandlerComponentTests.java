a2aae416a4c8d763a515756d428c6d95aef6ffef
Jacek Milewski <jacek.milewski@orange.com>
2016-05-12 20:05:23 +0200

    component with OK result

--- GitHubFacadeService/src/test/java/com/looksok/ExceptionHandlerComponentTests.java
+++ GitHubFacadeService/src/test/java/com/looksok/ExceptionHandlerComponentTests.java
@@ -4,10 +4,13 @@ import com.looksok.application.GitHubFacadeServiceApplication;
 import com.looksok.controller.RepoController;
 import com.looksok.service.repo.details.RepoDetailsService;
 import com.looksok.service.repo.details.exception.RepoNotFoundException;
+import com.looksok.service.repo.details.model.GitHubRepoModelSimple;
+import com.looksok.service.repo.details.model.RepoDetailsModel;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,7 +22,8 @@ import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = GitHubFacadeServiceApplication.class)
@@ -42,8 +46,8 @@ public class ExceptionHandlerComponentTests {
     public void returnsNotFoundStatusOnRepoNotFoundException() throws Exception {
         when(repoDetailsServiceMock.requestRepoDetails(anyString(), anyString())).thenThrow(RepoNotFoundException.class);
 
-        this.mockMvc
-                .perform(get("/repositories/user/repo") )
+        mockMvc
+                .perform(get("/repositories/user/repo").accept(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isNotFound());
     }
 
@@ -51,17 +55,39 @@ public class ExceptionHandlerComponentTests {
     public void returnsBadRequestStatusOnIllegalArgumentException() throws Exception {
         when(repoDetailsServiceMock.requestRepoDetails(anyString(), anyString())).thenThrow(IllegalArgumentException.class);
 
-        this.mockMvc
-                .perform(get("/repositories/user/repo") )
+        mockMvc
+                .perform(get("/repositories/user/repo").accept(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
-    public void returnsServiceUnavailableOnGitHubUnavailableException() throws Exception {
+    public void returnsServiceUnavailableOnEmptyOptional() throws Exception {
         when(repoDetailsServiceMock.requestRepoDetails(anyString(), anyString())).thenReturn(Optional.empty());
 
-        this.mockMvc
-                .perform(get("/repositories/user/repo") )
+        mockMvc
+                .perform(get("/repositories/user/repo").accept(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isServiceUnavailable());
+    }
+
+    @Test
+    public void returnsOkWithResult() throws Exception {
+        GitHubRepoModelSimple gitHubModelMock = mock(GitHubRepoModelSimple.class);
+        when(gitHubModelMock.getCreated_at()).thenReturn("2011-11-11T12:12:12Z");
+        when(gitHubModelMock.getDescription()).thenReturn("someDescription");
+        when(gitHubModelMock.getStargazers_count()).thenReturn(123);
+        when(gitHubModelMock.getFull_name()).thenReturn("LongName");
+        RepoDetailsModel repoDetailsModel = RepoDetailsModel.fromGitHubModel(gitHubModelMock);
+
+        when(repoDetailsServiceMock.requestRepoDetails(anyString(), anyString())).thenReturn(Optional.of(repoDetailsModel));
+
+        mockMvc
+            .perform(get("/repositories/user/repo").accept(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("createdAt").value("2011-11-11 13:12:12"))
+            .andExpect(jsonPath("description").value("someDescription"))
+            .andExpect(jsonPath("stars").value(123))
+            .andExpect(jsonPath("fullName").value("LongName"))
+            .andDo(print());
     }
 }
