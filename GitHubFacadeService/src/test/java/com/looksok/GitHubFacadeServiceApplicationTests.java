4cd11641c0dc2a6de5db041a4b363787b35d4126
Jacek Milewski <jacek.milewski@orange.com>
2016-05-12 19:35:19 +0200

    RestTemplate as a Singleton

--- GitHubFacadeService/src/test/java/com/looksok/GitHubFacadeServiceApplicationTests.java
+++ GitHubFacadeService/src/test/java/com/looksok/GitHubFacadeServiceApplicationTests.java
@@ -1,17 +1,51 @@
 package com.looksok;
 
 import com.looksok.application.GitHubFacadeServiceApplication;
+import com.looksok.controller.RepoController;
+import com.looksok.service.repo.details.RepoDetailsService;
+import com.looksok.service.repo.details.exception.RepoNotFoundException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = GitHubFacadeServiceApplication.class)
 @WebAppConfiguration
 public class GitHubFacadeServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {}
+    private RepoController repoController;
+
+    private MockMvc mockMvc;
+    private RepoDetailsService repoDetailsServiceMock;
+
+    @Before
+    public void setup() {
+        repoDetailsServiceMock = mock(RepoDetailsService.class);
+        repoController = new RepoController(repoDetailsServiceMock);
+        mockMvc = MockMvcBuilders.standaloneSetup(repoController).build();
+    }
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void returnsNotFoundStatusOnRepoNotFoundException() throws Exception {
+        when(repoDetailsServiceMock.requestRepoDetails(anyString(), anyString())).thenThrow(RepoNotFoundException.class);
+
+        this.mockMvc
+                .perform(get("/repositories/user/repo") )
+                .andExpect(status().isNotFound());
+    }
 }
