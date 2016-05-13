14d736ef51de327556bf90147e4c57f7b2ea7086
Jacek Milewski <jacek.milewski@orange.com>
2016-05-13 07:08:49 +0200

    verifying OK response provided from RestTemplate

--- GitHubFacadeService/src/test/java/com/looksok/ServiceE2eTests.java
+++ GitHubFacadeService/src/test/java/com/looksok/ServiceE2eTests.java
@@ -4,6 +4,7 @@ import com.looksok.application.GitHubFacadeServiceApplication;
 import com.looksok.controller.RepoController;
 import com.looksok.service.repo.details.RepoDetailsService;
 import com.looksok.service.repo.details.exception.GitHubUnavailableException;
+import com.looksok.service.repo.details.model.GitHubRepoModelSimple;
 import com.looksok.service.rest.RestTemplateSingleton;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,16 +14,23 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = GitHubFacadeServiceApplication.class)
@@ -39,18 +47,45 @@ public class ServiceE2eTests {
     @InjectMocks
     RepoDetailsService repoDetailsServiceMock;
 
+    @Mock
+    private RestTemplate restTemplateMock;
+
     @Autowired
     RepoController repoController;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
+        when(restTemplateSingletonMock.getInstance()).thenReturn(restTemplateMock);
         mockMvc = MockMvcBuilders.standaloneSetup(repoController).build();
     }
 
     @Test
+    public void returnsOkWithResult() throws Exception {
+        GitHubRepoModelSimple gitHubModelStub = mock(GitHubRepoModelSimple.class);
+        when(gitHubModelStub.getCreated_at()).thenReturn("2011-11-11T12:12:12Z");
+        when(gitHubModelStub.getDescription()).thenReturn("someDescription");
+        when(gitHubModelStub.getStargazers_count()).thenReturn(123);
+        when(gitHubModelStub.getFull_name()).thenReturn("LongName");
+
+        when(restTemplateMock.exchange(anyObject(), anyObject(), anyObject(), eq(GitHubRepoModelSimple.class)))
+            .thenReturn(new ResponseEntity<>(gitHubModelStub, HttpStatus.OK));
+
+        mockMvc
+                .perform(get("/repositories/user/repo").accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("createdAt").value("2011-11-11 13:12:12"))
+                .andExpect(jsonPath("description").value("someDescription"))
+                .andExpect(jsonPath("stars").value(123))
+                .andExpect(jsonPath("fullName").value("LongName"))
+                .andDo(print());
+    }
+
+    @Test
     public void returnsServiceUnavailableOnGitHubUnavailableException() throws Exception {
-        when(restTemplateSingletonMock.getInstance()).thenThrow(GitHubUnavailableException.class);
+        when(restTemplateMock.exchange(anyObject(), anyObject(), anyObject(), eq(GitHubRepoModelSimple.class)))
+                .thenThrow(GitHubUnavailableException.class);
 
         mockMvc
                 .perform(get("/repositories/user/repo").accept(MediaType.APPLICATION_JSON_UTF8))
@@ -59,7 +94,8 @@ public class ServiceE2eTests {
 
     @Test
     public void returnsServiceUnavailableOnRestClientException() throws Exception {
-        when(restTemplateSingletonMock.getInstance()).thenThrow(RestClientException.class);
+        when(restTemplateMock.exchange(anyObject(), anyObject(), anyObject(), eq(GitHubRepoModelSimple.class)))
+                .thenThrow(RestClientException.class);
 
         mockMvc
                 .perform(get("/repositories/user/repo").accept(MediaType.APPLICATION_JSON_UTF8))
